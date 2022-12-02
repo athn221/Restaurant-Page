69e88c59d40bec830eed9be2612a7b9f4d7bd7f3
david <leachda12@gmail.com>
2022-12-01 23:25:32 -0500

    salvaging some of code from previous project

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-// Time to lay this back out
+/* ---------- Cart ---------- */
+
+function shoppingCart() {
+    if (isVisible == false) {
+        isVisible = true
+        cart.classList.replace("shopping-cart", "sliding-cart")
+    } else if (isVisible == true) {
+        isVisible = false
+        cart.classList.replace("sliding-cart","shopping-cart")
+    }
+}
